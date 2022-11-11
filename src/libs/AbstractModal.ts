e51682f198eea7f3439b78ad1c190d611d135584
Yann <yann24.bonnau@hotmail.fr>
2022-11-11 21:13:14 +0100

    Fix interaction > 5

--- src/libs/AbstractModal.ts
+++ src/libs/AbstractModal.ts
@@ -141,7 +141,7 @@ export abstract class AbstractModal<T> {
       );
     } else if (Object.keys(form).length >= this.totalNumberElements) {
       components.push(
-        this.getInteractionStep(),
+        ...this.getInteractionStep(),
         new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
           new ButtonBuilder()
             .setLabel('Créer ou mettre à jour')
@@ -161,9 +161,14 @@ export abstract class AbstractModal<T> {
     });
   }
 
-  protected getInteractionStep(): ActionRowBuilder<MessageActionRowComponentBuilder> {
-    const row = new ActionRowBuilder<MessageActionRowComponentBuilder>();
+  protected getInteractionStep(): ActionRowBuilder<MessageActionRowComponentBuilder>[] {
+    const rowArray = [];
+    let row = new ActionRowBuilder<MessageActionRowComponentBuilder>();
     for (let modalNumber = 0; modalNumber < this.config.length; modalNumber++) {
+      if (modalNumber % 5 === 0) {
+        rowArray.push(row);
+        row = new ActionRowBuilder<MessageActionRowComponentBuilder>();
+      }
       row.addComponents(
         new ButtonBuilder()
           .setCustomId(`servers-modalButton-${modalNumber}`)
@@ -171,6 +176,6 @@ export abstract class AbstractModal<T> {
           .setStyle(ButtonStyle.Primary),
       );
     }
-    return row;
+    return rowArray;
   }
 }
